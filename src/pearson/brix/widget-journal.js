ba5dcc8f9cac2b2e449afc856c71d65e5a6e7eab
seannives <seann.ives@gmail.com>
2014-01-10 13:45:02 -0500

    state restoration for journal

--- src/pearson/brix/widget-journal.js
+++ src/pearson/brix/widget-journal.js
@@ -4,11 +4,12 @@
  *
  * @fileoverview Implementation of the Journal bric.
  *
- * The Journal bric displays an textbox that allows one or two paragraphs
- * to be entered and submitted.
+ * The Journal bric displays an textbox that allows text to be entered and
+ * submitted.
  *
  * Created on       November 17, 2013
  * @author          Michael Jay Lippert
+ *                  Seann Ives
  *
  * @copyright (c) 2013 Pearson, All rights reserved.
  *
@@ -17,11 +18,6 @@
 goog.provide('pearson.brix.Journal');
 
 goog.require('goog.debug.Logger');
-
-goog.require('goog.dom');
-goog.require('goog.dom.query');
-goog.require('goog.events.EventHandler');
-goog.require('goog.events.KeyHandler');
 
 goog.require('pearson.brix.HtmlBric');
 goog.require('pearson.utils.IEventManager');
@@ -104,7 +100,7 @@ pearson.brix.Journal = function (config, eventManager, bricWorks)
      * (e.g. the sequence node id)
      * @private
      * @type {string}
-     */
+     */  
     this.journalId_ = config.journalId;
 
     /**
@@ -129,6 +125,13 @@ pearson.brix.Journal = function (config, eventManager, bricWorks)
     this.submitButton = /**@type {!pearson.brix.Button}*/
                         (bricWorks.createBric(pearson.brix.BricTypes.BUTTON, submitBtnConfig));
 
+    /**
+     * List of responses that have been received for all submitted
+     * scoring requests.
+     * @private
+     * @type {Array.<!pearson.brix.Journal.ResponseRecord>}
+     */
+    this.responses_ = [];
     /**
      * The event manager to use to publish (and subscribe to) events for this bric
      * @type {!pearson.utils.IEventManager}
@@ -161,10 +164,10 @@ pearson.brix.Journal = function (config, eventManager, bricWorks)
      * Information about the last drawn instance of this bric (from the draw method)
      * @type {Object}
      */
-    this.lastdrawn =
+    this.lastdrawn_ =
         {
             container: null,
-            widgetGroup: null,
+            bricGroup: null,
         };
 }; // end of Journal constructor
 goog.inherits(pearson.brix.Journal, pearson.brix.HtmlBric);
@@ -227,9 +230,9 @@ pearson.brix.Journal.getEventTopic = function (eventName, instanceId)
  ****************************************************************************/
 pearson.brix.Journal.prototype.handleSubmitRequested_ = function ()
 {
-    this.logger_.fine('Journal: handling submit requested');
+    this.logger_.fine('handling submit requested');
 
-    var entry = this.lastdrawn.widgetGroup.select("textarea.entry");
+    var entry = this.lastdrawn_.bricGroup.select("textarea.entry");
     var entryText = entry.node().value;
 
     var submitAnsDetails =
@@ -246,6 +249,30 @@ pearson.brix.Journal.prototype.handleSubmitRequested_ = function ()
     this.eventManager.publish(this.submitScoreRequestEventId, submitAnsDetails);
 };
 
+/**
+ * Record of the details of a submitted choice and the response received.
+ * We're using the alwaysCorrect assessmentType for this so, barring
+ * studentSubmission, the response will always be the same.  However, we
+ * still want to know that we've gotten a response and this allows us to
+ * alter alwaysCorrect later without also having to clean Journal up too much.
+ *
+ * @typedef {Object} pearson.brix.Journal.ResponseRecord
+ * @property {{key: string}}
+ *                      studentSubmission   -The answer object sent for evaluation
+ * @property {number}   correctness         -correctness of the choice 0 incorrect
+ *                                           to 1 completely correct (alwaysCorrect
+ *                                           always returns 1)
+ * @property {string}   feedback            -Feedback about the choice (generic feedback
+ *                                           that journal currently ignores)
+ * @property {number}   attemptsMade        -what attempt at answering the question
+ *                                           this submission was.
+ * @property {{key: string, feedback: string}|undefined}
+ *                      correctAnswer       -optional property which is present only
+ *                                           when the student submission was not
+ *                                           correct AND no more attempts are allowed.
+ */
+pearson.brix.Journal.ResponseRecord;
+
 /* **************************************************************************
  * Journal.handleSubmitResponse_                                       */ /**
  *
@@ -261,30 +288,25 @@ pearson.brix.Journal.prototype.handleSubmitResponse_ = function (responseDetails
     this.logger_.fine('handling submit response');
     this.logger_.finer('responseDetails: ' + JSON.stringify(responseDetails));
 
-    // @todo Do we want to display some acknowledgement that the submission was
-    // successful?
-    // I'm adding this here just so this skeleton has some indication that
-    // the response was called.
-    var feedback = this.lastdrawn.widgetGroup.select("div.feedback");
-    feedback.append('div')
-        .attr('class', 'feedback-correct')
-        .text('Your entry is saved.');
-};
+    // Extract the info from the responseDetails that we need in the response record
+    /** @type {pearson.brix.MultipleChoiceQuestion.ResponseRecord} */
+    var respRec =
+        {
+            studentSubmission: responseDetails['submitDetails'].entry,
+            correctness: responseDetails['correctness'],
+            feedback: responseDetails['feedback'],
+            attemptsMade: responseDetails['attemptsMade']
+        };
 
-/* **************************************************************************
- * Journal.toggleEnabler                                               */ /**
- *
- * Not sure what a toggleEnabler does, this is obviously under development
- * but having a completely empty description causes an error when creating
- * jsdoc. Therefore this paragraph. -mjl
- */
-pearson.brix.Journal.prototype.toggleEnabler = function ()
-{
-    this.logger_.fine('Journal: handling submit requested');
+    if (responseDetails.correctAnswer)
+    {
+        respRec.correctAnswer = responseDetails.correctAnswer;
+    }
 
-    var entry = this.lastdrawn.widgetGroup.select("textarea.entry");
-    var entryText = entry.node().value;
+    // add this response to the list of responses
+    this.responses_.push(respRec);
 
+    this.redrawFeedback_();    
 };
 
 /* **************************************************************************
@@ -303,6 +325,68 @@ pearson.brix.Journal.prototype.getId = function ()
     return this.jrnlId_;
 };
 
+/**
+ * Journal state object. This object represents the state
+ * of a Journal bric and is returned by getState and is the
+ * parameter to setState.
+ *
+ * @typedef {Object} pearson.brix.Journal.StateObject
+ * @property {Array.<!pearson.brix.Journal.ResponseRecord>}
+ *                      submissions         -The collection of responses to
+ *                                           choices previously submitted.
+ */
+pearson.brix.Journal.StateObject;
+
+/* **************************************************************************
+ * Journal.getState                                     */ /**
+ *
+ * @inheritDoc
+ * @export
+ * @description The following is here until jsdoc supports the inheritDoc tag.
+ * Get a state object that represents the current state of this object and
+ * can be passed to restoreState.
+ *
+ * @returns {!pearson.brix.Journal.StateObject} Object that
+ *          when passed back to this type of object's restoreState method
+ *          will set its state to match the current state of this object.
+ *
+ ****************************************************************************/
+pearson.brix.Journal.prototype.getState = function ()
+{
+    throw new Error('getState has not yet been implemented on Journals');
+};
+
+/* **************************************************************************
+ * Journal.restoreState                                 */ /**
+ *
+ * @inheritDoc
+ * @export
+ * @description The following is here until jsdoc supports the inheritDoc tag.
+ * Restores the state of this object to match the state object given.
+ *
+ * @param {!Object} state   -Object returned by the call to getState on
+ *                           this type of an object representing the state
+ *                           to be restored.
+ *
+ ****************************************************************************/
+pearson.brix.Journal.prototype.restoreState = function (state)
+{
+    // @note: is this shallow array copy sufficient, or do we need to a deep copy? -mjl
+    this.responses_ = state['submissions'].slice();
+
+    this.attemptsMade_ = 0;
+    if (this.responses_.length !== 0)
+    {
+        this.attemptsMade_ = this.responses_[this.responses_.length - 1]['attemptsMade'];
+    }
+
+    // If we're drawn, we need to redraw
+    if (this.lastdrawn_.container != null)
+    {
+        this.redrawFeedback_();
+    }
+};
+
 /* **************************************************************************
  * Journal.draw                                                        */ /**
  *
@@ -316,35 +400,42 @@ pearson.brix.Journal.prototype.getId = function ()
  ****************************************************************************/
 pearson.brix.Journal.prototype.draw = function (container)
 {
-    this.logger_.fine('Journal: drawing');
+    this.logger_.fine('drawing');
 
     var that = this;
 
-    this.lastdrawn.container = container;
+    this.lastdrawn_.container = container;
 
     // make a div to hold the journal question
-    var widgetGroup = container.append('div')
+    var bricGroup = container.append('div')
         .attr('class', 'brixJournal');
 
     // use a fieldset (although w/o a form) to group the title
-    var jCntr = widgetGroup.append('fieldset');
+    var jCntr = bricGroup.append('fieldset');
 
     var title = jCntr.append('legend')
         .attr('class', 'title')
         .html(this.title_);
 
     // make a div to hold the textarea so we can size it appropriately
-    var areaCntr = widgetGroup.append('div')
+    var areaCntr = bricGroup.append('div')
         .attr('class', 'journalTextarea');
 
     var textentry = areaCntr.append('textarea')
         .attr('class', 'entry')
-        .attr('id', this.getId())
         .attr('placeholder', 'The response entered here will be saved to your notes and may be collected by your instructor if he/she requires it.')
         .attr('rows', '7');
 
-    // We need a block container for the submit button and the attempts
-    var submitAndAttemptsCntr = widgetGroup.append('div');
+    if (this.responses_.length !== 0)
+    {
+        // then get the last response and stick it in the textarea
+        var lastResponse = this.responses_[this.responses_.length - 1];
+        textentry.property('value', lastResponse.studentSubmission.entry);
+        textentry.attr('disabled', 'disabled');
+    }
+
+    // we need a block container for the submit button and the attempts
+    var submitAndAttemptsCntr = bricGroup.append('div');
 
     // draw the submit button below
     var submitButtonCntr = submitAndAttemptsCntr.append('div')
@@ -356,21 +447,69 @@ pearson.brix.Journal.prototype.draw = function (container)
     var attemptsCntr = submitAndAttemptsCntr.append('span')
         .attr('class', 'attempts');
 
-    // make a target for feedback when the question is answered
-    widgetGroup.append('div')
-        .attr('class', 'feedback');
-
-    // listen for keyboard events on the textarea and enable submit
-    var textArea = goog.dom.getElement(this.getId());
-    var keyHandler = new goog.events.KeyHandler(textArea);
-    goog.events.listen(keyHandler,
-        goog.events.KeyHandler.EventType.KEY,
-        function(e)
+    // If we haven't submitted, listen for keyboard events on the
+    // textarea and enable submit
+    if (this.responses_.length === 0)
+    {
+        textentry.on('keypress', function ()
         {
             that.submitButton.setEnabled(true);
         });
+    }
 
-    this.lastdrawn.widgetGroup = widgetGroup;
+    this.lastdrawn_.bricGroup = bricGroup;
+
+    // Drawing feedback depends on this.lastdrawn_.bricGroup
+    this.drawFeedback_(bricGroup);
 
 }; // end of Journal.draw()
 
+/* **************************************************************************
+ * Journal.drawFeedback_                                               */ /**
+ *
+ * Draw the feedback from the responses to prior attempts to answer this
+ * question.
+ * @private
+ *
+ * @param {!d3.selection}   cntr   -The container html element to append
+ *                                  the feedback to.
+ *
+ ****************************************************************************/
+pearson.brix.Journal.prototype.drawFeedback_ = function (cntr)
+{
+    this.logger_.fine('drawing feedback');
+    // make a target for feedback when the question is answered
+    cntr.append('div')
+        .attr('class', 'feedback');
+
+    this.redrawFeedback_();
+};
+
+/* **************************************************************************
+ * Journal.redrawFeedback_                              */ /**
+ *
+ * Update the displayed response feedback.
+ * @private
+ *
+ ****************************************************************************/
+pearson.brix.Journal.prototype.redrawFeedback_ = function ()
+{
+    var feedbackCntr = this.lastdrawn_.bricGroup.select('div.feedback');
+
+    // Currently we only display the feedback from the last response
+    // so 1st remove any feedback being displayed
+    var prevFeedback = this.lastdrawn_.bricGroup.selectAll('div.feedback > *');
+    prevFeedback.remove();
+
+    // If there's no responses then there's no feedback
+    if (this.responses_.length === 0)
+    {
+        return;
+    }
+
+    // If there is a response, provide the default feedback
+    feedbackCntr.append('div')
+        .attr('class', 'feedback-correct')
+        .text('Your entry is saved.');
+
+};
